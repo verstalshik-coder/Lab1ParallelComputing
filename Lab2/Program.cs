1834891dba6f1696ee0525fa3a5ef7b0fd113156
rekrutmaster <rekrutmaster@gmail.com>
2022-06-20 22:44:50 +0300

    Lab2 full

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab2.resource;
+
+var numbers = Enumerable.Range(1, 4);
+
+using (MyThreadPool thread_pool = new(2))
+{    
+    foreach (var num in numbers)
+    {
+        thread_pool.execute(num, obj =>
+            {
+                var temp = (int) obj!;
+                Console.WriteLine(Math.Pow(temp, 2));
+                Thread.Sleep(2000);
+            });
+    }
+
+    Console.ReadLine();
+}
